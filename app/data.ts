c8b423705ff54e7fe552d1b384be382edfe685cb
nezrayy <fanez.fthd@gmail.com>
2024-08-29 13:44:52 +0700

    progress home

---
+++ app/data.ts
@@ -0,0 +1,44 @@
+export default [
+    {
+        img: "/posters/interstellar.jpg",
+        year: 2014,
+        title: "Interstellar",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+        rating: 3
+    },
+    {
+        img: "/posters/deadpool_wolverine.jpg",
+        year: 2024,
+        title: "Deadpool and Wolverine",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+        rating: 4,
+    },
+    {
+        img: "/posters/alien_romulus.jpg",
+        year: 2024,
+        title: "Alien: Romulus",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+        rating: 5,
+    },
+    {
+        img: "/posters/twisters.jpg",
+        year: 2024,
+        title: "Twisters",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+        rating: 4,
+    },
+    {
+        img: "/posters/thedarkrises.jpg",
+        year: 2024,
+        title: "Batman: The Dark Knight Rises",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+        rating: 5,
+    },
+    {
+        img: "/posters/the_crow.jpg",
+        year: 2024,
+        title: "The Crow",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+        rating: 4,
+    }
+]
